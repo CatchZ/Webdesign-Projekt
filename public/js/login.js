a42bd184aba8fa3087ebe3f80abe571d4c0454db
RealAgnetha <45121184+RealAgnetha@users.noreply.github.com>
2021-11-10 21:41:10 +0100

    display whos logged in

--- public/js/login.js
+++ public/js/login.js
@@ -4,14 +4,16 @@ const user =
         password: "hunter2"
     };
 
+myStorage = localStorage;
+
 function logIn() {
     let username = document.getElementById('email').value;
     let password = document.getElementById('passwort').value;
 
     if (username === user.username && password === user.password) {
         window.location.replace("../public/map.html");
-        document.getElementById("loginFailed").innerHTML = "Username oder Passwort richtig";
-
+        localStorage.setItem("username", user.username);
+        localStorage.setItem("password", user.password);
     } else {
         document.getElementById("loginFailed").innerHTML = "Username oder Passwort falsch";
     }
