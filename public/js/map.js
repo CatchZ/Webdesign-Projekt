7a788d68c91ce6b5cf988f1e1140cd065d329b4f
CatchZ <maximilian.ziemann@gmx.de>
2021-11-18 13:37:18 +0100

    änderung an map

--- public/js/map.js
+++ public/js/map.js
@@ -6,13 +6,13 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png	', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(mymap);
 
-let visitedCountries = ['DE', 'US', 'RU']; <!--"localStorageSetUp()"-->
+let visitedCountries = localStorageSetUp();
 
 const loadData = async () => {
     const data = await fetch('https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_50m_admin_0_countries.geojson');
     return data.json();
 }
-
+// dynamisch gemacht
 const displayData = async () => {
     const geoJson = await loadData();
     const filteredData = {
@@ -25,18 +25,27 @@ const displayData = async () => {
 
 displayData();
 
-/*
-function FilterSelection(){
-    const newElem = document.getElementsByName("reiseziel").value;
-    localStorage.setItem(reiseziel,newElem);
-    const obj = JSON.parse(localStorage.getItem("reiseziel"));
-    reiseziel  = JSON.parse(localStorage.getIteam('reiseziel');
 
-}
+/**
+ * richtet Local Storage Variable reiseziel ein oder gibt aktuellen inhalt zurück
+ * @returns {string}
+ */
 function  localStorageSetUp(){
-    let x ="";
-    if(localStorage.hasOwnProperty("reiseziel") ){x = JSON.parse(localStorage.getIteam('reiseziel')}
-    else { "";}
-    return x
+  return ['DE', 'US', 'RU'];
 }
-*/
+
+/**
+ * updated map mit Local Storage informationen
+ * @constructor
+ */
+function UpdateMap(){
+    const displayData = async () => {
+        const geoJson = await loadData();
+        const filteredData = {
+            ...geoJson,
+            features: geoJson.features.filter(feature =>
+                !visitedCountries.includes(feature.properties.iso_a2))
+        };
+        L.geoJSON(filteredData).addTo(mymap);
+    }
+}
