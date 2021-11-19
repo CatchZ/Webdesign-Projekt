a6056acdb4204ca52711b410cd22055e3faeca21
CatchZ <maximilian.ziemann@gmx.de>
2021-11-19 13:35:52 +0100

    map versuche

--- public/js/map.js
+++ public/js/map.js
@@ -6,7 +6,11 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png	', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(mymap);
 
-let visitedCountries = localStorageSetUp();
+const laender = ['DE', 'US', 'RU'];
+laender.push(`FR`);
+window.localStorage.setItem("Laender",JSON.stringify(laender));
+const laender2 = JSON.parse(localStorage.getItem("Laender"));
+const visitedCountries =laender2;// /*testvar2;*/localStorageSetUp();
 
 const loadData = async () => {
     const data = await fetch('https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_50m_admin_0_countries.geojson');
@@ -39,13 +43,9 @@ function  localStorageSetUp(){
  * @constructor
  */
 function UpdateMap(){
-    const displayData = async () => {
-        const geoJson = await loadData();
-        const filteredData = {
-            ...geoJson,
-            features: geoJson.features.filter(feature =>
-                !visitedCountries.includes(feature.properties.iso_a2))
-        };
-        L.geoJSON(filteredData).addTo(mymap);
-    }
+   var x=document.getElementById("reiseziel")
+   var y=  JSON.parse(window.localStorage.getItem("Laender"));
+   window.localStorage.removeItem("Laender");
+   y.push(x);
+   window.localStorage.setItem("Laender",JSON.parse(y));
 }
