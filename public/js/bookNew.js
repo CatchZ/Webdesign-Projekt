ea6d36bfa89c9970cd298e6db5054951f2ddb5cc
CarolinaTrack <carolinatrack@googlemail.com>
2021-11-20 01:48:06 +0100

    fix display bug table

--- public/js/bookNew.js
+++ public/js/bookNew.js
@@ -1,21 +1,3 @@
-/*var beispielreise1 = {
-    'reisename': 'Thailandreise',
-    'reiseziel': 'Thailand',
-    'reisestart': 'start',
-    'reiseende': 'ende'
-}
-var beispielreise2 = {
-    'reisename': 'Party in Prague',
-    'reiseziel': 'Tschechien',
-    'reisestart': 'start',
-    'reiseende': 'ende'
-}
-alleReisen.push(beispielreise1);
-alleReisen.push(beispielreise2);
-
- */
-
-
 var addReise = function (name, ziel, start, ende) {
     var alleReisen = JSON.parse(localStorage.getItem('reisenArray')) || [];
 
@@ -36,19 +18,9 @@ var addReise = function (name, ziel, start, ende) {
 
 function myFunction(){
     var getReise = JSON.parse(localStorage.getItem('reisenArray'));
-
-    document.getElementById("testForm").innerHTML = getReise[getReise.length-1].reisename;
-
+   // document.getElementById("testForm").innerHTML = getReise[getReise.length-1].reisename;
 }
 
 /*
-function myFunction() {
-var table = document.getElementById("tabelleReiseuebersicht");
-var row = table.insertRow(0);
-var cell1 = row.insertCell(0);
-var cell2 = row.insertCell(1);
-cell1.innerHTML = "NEW CELL1";
-cell2.innerHTML = "NEW CELL2";
-}
 
-*/
+ */
--- public/js/manageReisen.js
+++ public/js/manageReisen.js
@@ -1,15 +1,15 @@
 var getReise = JSON.parse(localStorage.getItem('reisenArray'));
-var btnReiseLoeschen = document.createElement('button');
-
+//var btnReiseLoeschen = document.createElement('button');
 
 function showReisen() {
     var table = document.getElementById("tabelleReiseuebersicht");
 
-    for (let i=1; i<getReise.length;i++) {
-        var btnTest =  document.createElement('button');
-        btnTest.setAttribute('onclick','myFunction');
+    for (let i=0; i<=getReise.length;i++) {
+    //for (let i=getReise.length-1; i>0;i--) {
+        //var btnTest =  document.createElement('button');
+        //btnTest.setAttribute('onclick','myFunction');
 
-        var row = table.insertRow(i);
+        var row = table.insertRow(-1);
         var cell1 = row.insertCell(0);
         var cell2 = row.insertCell(1);
         var cell3 = row.insertCell(2);
@@ -17,19 +17,34 @@ function showReisen() {
         var cell5 = row.insertCell(4);
         //var cell6 = row.insertCell(5);
 
-
         cell1.innerHTML = getReise[i].reisename;
+        //var reisenametemp = cell1.innerHTML;
         cell2.innerHTML = getReise[i].reiseziel;
         cell3.innerHTML = getReise[i].reisestart;
         cell4.innerHTML = getReise[i].reiseende;
         //cell5.innerHTML = '<input type="button" value="Löschen" onClick="reiseLoeschen(this)">';
         cell5.innerHTML = '<button type="button" onclick="reiseLoeschen(this)">Löschen</button>';
     }
-
 }
-function reiseLoeschen(r) {
+
+function reiseLoeschen(r, reisename) {
     var x = r.parentNode.parentNode.rowIndex;
     document.getElementById("tabelleReiseuebersicht").deleteRow(x);
+
+    localStorage.removeItem(reisename);
+    /*
+    var reisen = JSON.parse(localStorage.getItem('reisenArray'));
+
+    for (var j=0; j<reisen.length; j++) {
+        var reisen = JSON.parse(reisen[i]);
+        if (reisen.reisename === reisename) {
+            reisen.splice(j, 1);
+        }
+    }
+    reisen = JSON.stringify(reisen); //Restoring object left into items again
+    localStorage.setItem("reisenArray", reisen);
+     */
+
 }
 
 
