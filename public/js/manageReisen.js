c4e7dac96e80dc72fc9c0a85d71643d22eac6b73
CarolinaTrack <carolinatrack@googlemail.com>
2021-11-20 05:45:20 +0100

    am knobeln mit editable cells

--- public/js/manageReisen.js
+++ public/js/manageReisen.js
@@ -15,22 +15,26 @@ function showReisen() {
         var cell3 = row.insertCell(2);
         var cell4 = row.insertCell(3);
         var cell5 = row.insertCell(4);
-        //var cell6 = row.insertCell(5);
+        var cell6 = row.insertCell(5);
 
+        //cell1.innerHTML = '<p id="pReisename" contenteditable="true"></p>';
         cell1.innerHTML = getReise[i].reisename;
-        //var reisenametemp = cell1.innerHTML;
+
+        // document.getElementById("pReisename").innerHTML = getReise[i].reisename;
         cell2.innerHTML = getReise[i].reiseziel;
         cell3.innerHTML = getReise[i].reisestart;
         cell4.innerHTML = getReise[i].reiseende;
-        //cell5.innerHTML = '<input type="button" value="Löschen" onClick="reiseLoeschen(this)">';
-        cell5.innerHTML = '<button type="button" onclick="reiseLoeschen(this)">Löschen</button>';
+        cell5.innerHTML = '<button type="button" onclick="reiseAktualisieren(this)">Aktualisieren</button>';
+        cell6.innerHTML = '<button type="button" onclick="reiseLoeschen(this)">Löschen</button>';
     }
 }
 
 function reiseLoeschen(r) {
+    //löscht von der Tabelle
     var x = r.parentNode.parentNode.rowIndex;
     document.getElementById("tabelleReiseuebersicht").deleteRow(x);
 
+    //löscht aus dem localStorage
     var reisenAlt = JSON.parse(localStorage.getItem('reisenArray')) || [];
 
     x-=2;
@@ -39,9 +43,10 @@ function reiseLoeschen(r) {
     localStorage.setItem('reisenArray', JSON.stringify(reisenAlt));
 
     var reisenArray = JSON.stringify(localStorage.getItem('reisenArray'));
-
 }
 
-
+function reiseAktualisieren(r){
+    //soll neue Daten aufnehmen und speichern
+}
 
 
