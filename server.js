f2390197ed3be1c799a54ec9b52896626af0a520
Carolina <45121184+RealAgnetha@users.noreply.github.com>
2021-12-14 16:07:03 +0100

    failed attemps server

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000
 
+app.get("/", function(req,res){
+    //res.send({headers: req.headers})
+    res.send("hallo");
+})
+
 app.listen(port, () => {
     console.log(`Reisen reisen listening at http://localhost:${port}`)
 })
