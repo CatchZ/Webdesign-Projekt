58e016e5be94991f61d62892754f151cb90f214d
Carolina <45121184+RealAgnetha@users.noreply.github.com>
2021-12-19 22:35:34 +0100

    fix error message username/passwort falsch

--- public/js/logOut.js
+++ public/js/logOut.js
@@ -1,8 +1,35 @@
+const BASE_URL = "https://reisen-reisen.herokuapp.com";
+
+async function displayLogOut() {
+    // let usernameLoggedIn = localStorage.getItem("username");
+    const loginUrl = `${BASE_URL}/loggedInUser`;
+
+    const response = await fetch(loginUrl, {
+        method: 'GET',
+        credentials: 'include',
+        body: JSON.stringify({username})
+    });
+    document.getElementById("loggedInUser").innerHTML = "Eingeloggt als " + username;
+    return response.status === 200;
+}
+
+function logout() {
+    localStorage.clear();
+    window.location.replace("../index.html");
+}
+
+
+/*
 function displayLogOut() {
-    let usernameLoggedIn = localStorage.getItem("username");
+    // let usernameLoggedIn = localStorage.getItem("username");
+    let usernameLoggedIn =
+
     document.getElementById("loggedInUser").innerHTML = "Eingeloggt als " + usernameLoggedIn;
 }
 function logout() {
     localStorage.clear();
     window.location.replace("../index.html");
 }
+
+
+ */
