ecd75e10185e6868094de045e74fe59435a5e242
Carolina <45121184+RealAgnetha@users.noreply.github.com>
2022-01-18 15:55:45 +0100

    mail test

---
+++ public/js/mailer.js
@@ -0,0 +1,36 @@
+/*document.getElementById("registrationForm2").addEventListener("submit", mail);
+
+"use strict";
+const nodemailer = require("nodemailer");
+
+// async..await is not allowed in global scope, must use a wrapper
+async function mail() {
+
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'wadtestwise2122@gmail.com',
+            pass: 'hunter2aberrueckwaerts'
+        }
+    });
+
+    let mailOptions = {
+        from: 'youremail@gmail.com',
+        to: 'myfriend@yahoo.com',
+        subject: 'Sending Email using Node.js',
+        text: 'That was easy!'
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+}
+
+mail().catch(console.error);
+
+ */
--- public/js/registrieren.js
+++ public/js/registrieren.js
@@ -6,7 +6,7 @@ var username = document.getElementById('email');
 var password = document.getElementById('passwort');
 */
 
-document.getElementById("registrationForm").addEventListener("submit", neuenUserRegistrieren)
+document.getElementById("registrationForm").addEventListener("submit", neuenUserRegistrieren);
 
 function neuenUserRegistrieren() {
     alert("Bestätigungsmail versendet. Bitte besuche dein Mailfach und bestätige deine Anmeldung innerhalb von zwei Tagen, um deine Registrierung abzuschließen.");
@@ -15,34 +15,3 @@ function neuenUserRegistrieren() {
       //  "Bitte besuche dein Mailfach und bestätige deine Anmeldung innerhalb von zwei Tagen, um deine Registrierung abzuschließen." ;
 }
 
-
-/*
-const regist = async (username, password) => {
-    const loginUrl = `${BASE_URL}/login`;
-
-    const response = await fetch(loginUrl, {
-        method: 'POST',
-        mode: 'cors',
-        credentials: 'include',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({username, password})
-    });
-    return response.status === 200;
-}
-
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-
-    regist(username.value, password.value)
-        .then(wasSuccessfulLogin => {
-            if (wasSuccessfulLogin) {
-                window.location.replace("../map.html");
-            } else {
-                document.getElementById("loginFailed").innerHTML = "Username oder Passwort falsch";
-            }
-        })
-});
-
- */
