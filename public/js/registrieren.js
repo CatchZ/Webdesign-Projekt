38f86f8ba0ab41571023020d3fa244f77d514538
Carolina <45121184+RealAgnetha@users.noreply.github.com>
2022-01-18 16:39:47 +0100

    payload testen

--- public/js/registrieren.js
+++ public/js/registrieren.js
@@ -1,12 +1,41 @@
-/*
+
 const BASE_URL = "https://reisen-reisen.herokuapp.com";
 
 const form = document.querySelector('form');
-var username = document.getElementById('email');
-var password = document.getElementById('passwort');
-*/
+var username = document.getElementById('emailReg');
+var password = document.getElementById('passwortReg');
 
-document.getElementById("registrationForm").addEventListener("submit", neuenUserRegistrieren);
+
+//document.getElementById("registrationForm").addEventListener("submit", neuenUserRegistrieren);
+
+const register = async (username, password) => {
+    const regUrl = `${BASE_URL}/registrationRequest`;
+
+    const response = await fetch(regUrl, {
+        method: 'POST',
+        mode: 'cors',
+        credentials: 'include',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({username, password})
+    });
+    return response.status === 200;
+}
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    register(username.value, password.value)
+        .then(wasSuccessful => {
+            if (wasSuccessful) {
+                alert("Klappt");
+                window.location.replace("../map.html");
+            } else {
+               alert("Etwas hat nicht geklappt");
+            }
+        })
+});
 
 function neuenUserRegistrieren() {
     alert("Bestätigungsmail versendet. Bitte besuche dein Mailfach und bestätige deine Anmeldung innerhalb von zwei Tagen, um deine Registrierung abzuschließen.");
