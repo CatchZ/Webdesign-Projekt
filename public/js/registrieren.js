fa2736397d4e3ace3934d55380efeefc77b02560
Carolina <45121184+RealAgnetha@users.noreply.github.com>
2022-01-22 12:33:56 +0100

    cors

--- public/js/registrieren.js
+++ public/js/registrieren.js
@@ -13,7 +13,7 @@ const register = async (username, password) => {
 
     const response = await fetch(regUrl, {
         method: 'POST',
-        mode: 'no-cors',
+        mode: 'cors',
         credentials: 'include',
         headers: {
             'Content-Type': 'application/json'
